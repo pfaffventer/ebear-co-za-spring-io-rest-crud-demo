ffa81479d73244c2eed02742de1b1af51706f5a8
pfaff <johanv@tru-cape.co.za>
2016-09-29 16:11:00 +0200

    Signed-off-by: pfaff <johanv@tru-cape.co.za>

--- ebear.co.za.spring.io.rest.crud.demo/src/main/java/ebear/co/za/spring/io/rest/crud/demo/supplier/controller/Controller_Supplier.java
+++ ebear.co.za.spring.io.rest.crud.demo/src/main/java/ebear/co/za/spring/io/rest/crud/demo/supplier/controller/Controller_Supplier.java
@@ -1,9 +1,14 @@
 package ebear.co.za.spring.io.rest.crud.demo.supplier.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ebear.co.za.spring.io.rest.crud.demo.base.model.LogRecord;
+import ebear.co.za.spring.io.rest.crud.demo.supplier.model.Supplier;
 import ebear.co.za.spring.io.rest.crud.demo.supplier.repository.CrudRepository_Supplier;
 
 @RestController
@@ -12,24 +17,107 @@ public class Controller_Supplier {
 	@Autowired
 	CrudRepository_Supplier supplierRepository;
 
-	@RequestMapping("/create")
+	/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	 * CREATE NEW SUPPLIER
+	 *
+	 * GET - create a default supplier supplierCode = ?
+	 */
+	@RequestMapping("/supplier/create/supplierCode")
 	public String create(String supplierCode) {
 		try {
+			supplierRepository.save(new Supplier(new LogRecord(System.getProperty("user.name")),supplierCode,"DESCRIPTION FOR (" + supplierCode + ")"));
 		} catch (Exception e) {
 			return "Error creating supplier: " + e.toString();
 		}
-		return "Supplier id: " + supplierCode + " succesfully created";
+		return "Supplier: " + supplierCode + " succesfully created";
 	}
 
-
-	@RequestMapping("/delete")
-	public String delete(String supplierCode) {
+	/*
+	 * POST - create new supplier = ?
+	 */
+	@RequestMapping("/supplier/create/")
+	public ResponseEntity<Supplier> create(@RequestBody Supplier newSupplier) {
+		Supplier supplier = null;
 		try {
+			supplier = supplierRepository.save(newSupplier);
+		} catch (Exception e) {
+			return new ResponseEntity<Supplier>(supplier, HttpStatus.NOT_EXTENDED);	
+		}
+		return new ResponseEntity<Supplier>(supplier, HttpStatus.OK);		
+	}
+
+	/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	 * DELETE SUPPLIER
+	 *
+	 * GET - delete a supplier where supplier_id = ? 
+	 */
+	@RequestMapping("/supplier/delete/id")
+	public String delete(long id) {
+		try {
+			supplierRepository.delete(supplierRepository.findById(id));
 		}
 		catch (Exception e) {
-			return "Error deleting supplier:" + e.toString();
+			return "Error deleting supplier: " + "[" + id + "] "+ e.toString();
 		}
-		return "Supplier id: " + supplierCode + " succesfully created";
+		return "Supplier: " + id + " succesfully deleted";
 	}
+
+	/*
+	 * GET - delete a supplier where supplier_code = ? 
+	 */
+	@RequestMapping("/supplier/delete/supplierCode")
+	public String delete(String supplierCode) {
+		try {
+			supplierRepository.delete(supplierRepository.findBySupplierCode(supplierCode));
+		}
+		catch (Exception e) {
+			return "Error deleting supplier: " + "[" + supplierCode + "] "+ e.toString();
+		}
+		return "Supplier: " + supplierCode + " succesfully deleted";
+	}
+
+
+	/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	 * READ SUPPLIER
+	 *
+	 * GET - read supplier where supplier_id = ?
+	 */
+	@RequestMapping("/supplier/read/id")
+	public ResponseEntity<Supplier> read(long id) {
+		Supplier supplier = null;
+		try {
+			supplier = supplierRepository.findById(id);
+			if(supplier==null) {
+				return new ResponseEntity<Supplier>(supplier, HttpStatus.NOT_FOUND);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<Supplier>(supplier, HttpStatus.NOT_EXTENDED);	
+
+		}
+		return new ResponseEntity<Supplier>(supplier, HttpStatus.OK);		
+	}
+
+	/*
+	 * GET - read supplier where supplier_code = ?
+	 */
+	@RequestMapping("/supplier/read/supplierCode")
+	public ResponseEntity<Supplier> read(String supplierCode) {
+		Supplier supplier = null;
+		try {
+			supplier = supplierRepository.findBySupplierCode(supplierCode);
+			if(supplier==null) {
+				return new ResponseEntity<Supplier>(supplier, HttpStatus.NOT_FOUND);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<Supplier>(supplier, HttpStatus.NOT_EXTENDED);	
+
+		}
+		return new ResponseEntity<Supplier>(supplier, HttpStatus.OK);		
+	}
+
+
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	//SAVE SUPPLIER
+
 
 }
--- ebear.co.za.spring.io.rest.crud.demo/src/test/java/ebear/co/za/spring/io/rest/crud/demo/ApplicationTests.java
+++ ebear.co.za.spring.io.rest.crud.demo/src/test/java/ebear/co/za/spring/io/rest/crud/demo/ApplicationTests.java
@@ -1,16 +1,66 @@
 package ebear.co.za.spring.io.rest.crud.demo;
 
 import org.junit.Test;
+
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import ebear.co.za.spring.io.rest.crud.demo.base.model.LogRecord;
+import ebear.co.za.spring.io.rest.crud.demo.supplier.model.Supplier;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
 
+	
+	public static final String REST_SERVICE_URI = "http://localhost:8080";
+
+	/*
+	 * TEST - create & delete
+	 */
 	@Test
-	public void contextLoads() {
+	public void testSupplierCreateDelete() {
+		RestTemplate restTemplate = new RestTemplate();
+		String result = "";
+		
+		System.out.println(":) Testing /supplier/create/supplierCode ***");
+		result = (String) restTemplate.getForObject(REST_SERVICE_URI + "/supplier/create/supplierCode?supplierCode=NEW777", String.class);
+		System.out.println(":) DONE TESTING: " + result);
+
+		System.out.println(":) Testing /supplier/delete/supplierCode ***");
+		result = (String) restTemplate.getForObject(REST_SERVICE_URI + "/supplier/delete/supplierCode?supplierCode=NEW777", String.class);
+		System.out.println(":) DONE TESTING: " + result);
+
+		System.out.println(":) Testing /supplier/create ***");
+		Supplier newSupplier = new Supplier(new LogRecord("test"),"NEW777","TEST CREATE");
+		Supplier supplier = restTemplate.postForObject(REST_SERVICE_URI + "/supplier/create/", newSupplier, Supplier.class);
+		System.out.println(":) DONE TESTING: " + supplier.getId());
+
+		System.out.println(":) Testing /supplier/delete/id ***");
+		result = (String) restTemplate.getForObject(REST_SERVICE_URI + "/supplier/delete/id?id=" + supplier.getId(), String.class);
+		System.out.println(":) DONE TESTING: " + result);
 	}
+
+	/*
+	 * TEST - read
+	 */
+	@Test
+	public void testSupplierReadID() {
+		RestTemplate restTemplate = new RestTemplate();
+
+		System.out.println(":) Testing /supplier/read/id ***");
+		restTemplate = new RestTemplate();
+		Supplier supplier = restTemplate.getForObject(REST_SERVICE_URI + "/supplier/read/id?id=1", Supplier.class);
+		System.out.println(":) DONE TESTING: " + supplier.getId());
+
+		System.out.println(":) Testing /supplier/read/supplierCode ***");
+		restTemplate = new RestTemplate();
+		supplier = restTemplate.getForObject(REST_SERVICE_URI + "/supplier/read/supplierCode?supplierCode=MSB001", Supplier.class);
+		System.out.println(":) DONE TESTING: " + supplier.getSupplierCode());
+	}
+
+
 
 }
